e4e64cd5dd00ad3038764e8879e99b22c6a00611
helolrodrigues <heloisa.rodrigues19@etec.sp.gov.br>
2025-08-08 17:20:49 -0300

    criação do componente e CSS

--- primeiro-teste/src/App.js
+++ primeiro-teste/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import HelloWorld from "./components/HelloWorld";
+import Frases from './components/Frases';
+import DigaMeuNome from './components/DigaMeuNome';
 
 function App() {
   return (
     <div className="App">
       <h1>MEU PRIMEIRO REACT</h1>
+      <HelloWorld/>
+      <Frases/>
+      <DigaMeuNome nome="Anderson"/>
+      
+
     </div>
   );
 }
